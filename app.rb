21f884a9b7b5cd845a5acfab6adfa64092f74ed0
Sam Escobar <sescobar8714@gmail.com>
2020-04-21 07:02:57 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do 
+        "My name is Sam"
+    end
+
+    get '/hometown' do 
+        "My hometown is Chicago"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Music Sounds Better With You"
+    end
+
 end
